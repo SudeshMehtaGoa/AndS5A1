5ced8b69b81c63a7c6e2704c5a10f4e48c05f506
Mehta Sudesh <Mehta@iazi.ch>
2018-01-02 22:03:58 +0100

    Animation Project

---
+++ app/src/main/java/com/example/mehta/ands5a1/AnimationActivity.java
@@ -0,0 +1,73 @@
+package com.example.mehta.ands5a1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class AnimationActivity extends AppCompatActivity implements Animation.AnimationListener{
+
+    TextView txtMessage;
+    Button btnStart;
+    Animation animFadein, animFadeout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animation);
+        txtMessage = (TextView) findViewById(R.id.txtMessage);
+        btnStart = (Button) findViewById(R.id.btnStart);
+
+        animFadeout= AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_out);
+
+        // load the animation
+        animFadein = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+
+        // set animation listener
+        animFadein.setAnimationListener(this);
+
+        // button click event
+        btnStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                txtMessage.setVisibility(View.VISIBLE);
+
+                // start the animation
+                txtMessage.startAnimation(animFadein);
+            }
+        });
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        // Take any action after completing the animation
+
+        // check for fade in animation
+        if (animation == animFadein) {
+            Toast.makeText(getApplicationContext(), "Animation Stopped",
+                    Toast.LENGTH_SHORT).show();
+            txtMessage.startAnimation(animFadeout);
+            animFadeout.start();
+        }
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
